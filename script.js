19321be7a14e03b30eb3812060a2a12cade86aa2
Kevin Clercin <kevin@9troisquarts.com>
2021-11-28 21:41:44 +0100

    feat:  Wave

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-  let ratio = 1;
-  let blueRatio = 1.5;
-  let blackRatio = 1.5;
-  let redRatio = 1.5;
-  let maxDistortionBlack = 30;
-  let maxDistortionBlue = 30;
-  let maxDistortionRed = 30;
+  "use strict";
+  var redMask = $('.red');
+  var ratio = 1;
+  var whiteMask = $('.white');
+  var blueMask = $('.blue');
+  var h = document.getElementsByTagName('h1')[0];
+  var redHistory = [];
 
   function toggleFullScreen() {
     if (!document.fullscreenElement) {
@@ -17,40 +17,6 @@ window.onload = function () {
     }
   }
 
-  function getRandomInt(max) {
-    return Math.floor(Math.random() * max);
-  }
-
-  $("#ratio").on("change", function() {
-    const value = $(this).val();
-    ratio = parseFloat(value, 10) || 1;
-  })
-
-  $("#blackRatio").on("change", function() {
-    const value = $(this).val();
-    blackRatio = parseFloat(value, 10);
-  })
-  $("#blueRatio").on("change", function() {
-    const value = $(this).val();
-    blueRatio = parseFloat(value, 10);
-  })
-  $("#redRatio").on("change", function() {
-    const value = $(this).val();
-    redRatio = parseFloat(value, 10);
-  })
-  $("#maxDistortionBlack").on("change", function() {
-    const value = $(this).val();
-    maxDistortionBlack = parseFloat(value, 10) || 20;
-  });
-  $("#maxDistortionBlue").on("change", function() {
-    const value = $(this).val();
-    maxDistortionBlue = parseFloat(value, 10) || 30;
-  });
-    $("#maxDistortionRed").on("change", function() {
-    const value = $(this).val();
-    maxDistortionRed = parseFloat(value, 10) || 25;
-  });
-
   $(document).on("keypress", function(e) {
     if(e.key == "c") {
       $("#configPanel").toggleClass("close")
@@ -60,70 +26,164 @@ window.onload = function () {
     }
   })
 
-  "use strict";
-  var h = document.getElementsByTagName('h1')[0];
-  var report = 0;
-  var blueCircles = $(".corners4")
-  var redCircles = $(".corners2")
-  var blackCircles = $(".corners3")
+  $("#ratio").on("change", function() {
+    const value = $(this).val();
+    ratio = parseFloat(value, 10) || 1;
+  })
+
+  function formatPoints(points, close) {
+  points = [...points];
+  // so that coords can be passed as objects or arrays
+  if (!Array.isArray(points[0])) {
+    points = points.map(({ x, y }) => [x, y]);
+  }
+
+  if (close) {
+    const lastPoint = points[points.length - 1];
+    const secondToLastPoint = points[points.length - 2];
+
+    const firstPoint = points[0];
+    const secondPoint = points[1];
+
+    points.unshift(lastPoint);
+    points.unshift(secondToLastPoint);
+
+    points.push(firstPoint);
+    points.push(secondPoint);
+  }
+
+  return points.flat();
+  }
+
+  function spline(points = [], tension = 1, close = false, cb) {
+    points = formatPoints(points, close);
+
+    const size = points.length;
+    const last = size - 4;
+
+    const startPointX = close ? points[2] : points[0];
+    const startPointY = close ? points[3] : points[1];
+
+    let path = "M" + [startPointX, startPointY];
+
+    cb && cb("MOVE", [startPointX, startPointY]);
+
+    const startIteration = close ? 2 : 0;
+    const maxIteration = close ? size - 4 : size - 2;
+    const inc = 2;
+
+    for (let i = startIteration; i < maxIteration; i += inc) {
+      const x0 = i ? points[i - 2] : points[0];
+      const y0 = i ? points[i - 1] : points[1];
+
+      const x1 = points[i + 0];
+      const y1 = points[i + 1];
+
+      const x2 = points[i + 2];
+      const y2 = points[i + 3];
+
+      const x3 = i !== last ? points[i + 4] : x2;
+      const y3 = i !== last ? points[i + 5] : y2;
+
+      const cp1x = x1 + ((x2 - x0) / 6) * tension;
+      const cp1y = y1 + ((y2 - y0) / 6) * tension;
+
+      const cp2x = x2 - ((x3 - x1) / 6) * tension;
+      const cp2y = y2 - ((y3 - y1) / 6) * tension;
+
+      path += "C" + [cp1x, cp1y, cp2x, cp2y, x2, y2];
+
+      cb && cb("CURVE", [cp1x, cp1y, cp2x, cp2y, x2, y2]);
+    }
+
+    return path;
+  }
 
   var soundAllowed = function (stream) {
     //Audio stops listening in FF without // window.persistAudioStream = stream;
     //https://bugzilla.mozilla.org/show_bug.cgi?id=965483
     //https://support.mozilla.org/en-US/questions/984179
-    h.setAttribute('style', 'opacity: 0;');
     window.persistAudioStream = stream;
+    h.innerHTML = "Thanks";
+    h.setAttribute('style', 'opacity: 0;');
     var audioContent = new AudioContext();
-    var audioStream = audioContent.createMediaStreamSource(stream);
+    var audioStream = audioContent.createMediaStreamSource( stream );
     var analyser = audioContent.createAnalyser();
     audioStream.connect(analyser);
     analyser.fftSize = 1024;
 
+    const numPoints = 200;
+    const offset = 500;
+    const angleStep = (Math.PI) / numPoints;
+
     var frequencyArray = new Uint8Array(analyser.frequencyBinCount);
-    //visualizer.setAttribute('viewBox', '0 0 255 255');
+
+    var p = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+
+    $("svg").append(p)
+
     var doDraw = function () {
-      requestAnimationFrame(doDraw);
-      analyser.getByteFrequencyData(frequencyArray);
-      const blackFrequency = frequencyArray.slice(0, 84).reduce((acc, value) => acc + value, 0);
-      const redFrequency = frequencyArray.slice(85, 169).reduce((acc, value) => acc + value, 0);;
-      const blueFrequency = frequencyArray.slice(170, 254).reduce((acc, value) => acc + value, 0);
+        requestAnimationFrame(doDraw);
+        analyser.getByteFrequencyData(frequencyArray);
 
-      let blueSkew = (blackFrequency / 500) * (blueRatio * ratio);
-      blueSkew = blueSkew - (blueSkew % 5)
-      if (blueSkew > maxDistortionBlue) blueSkew = maxDistortionBlue;
+        frequencyArray= frequencyArray.slice(0, numPoints)
+        let invertedPoints = []
+        var points = frequencyArray.reduce((acc, value, index) => {
+          if (index > 1 && index % 20 === 0) {
+            const theta = index * angleStep;
+            var length = Math.floor(value) - (Math.floor(value) % 5)
+            length = length * 1.5 * ratio;
+            const yRadius = Math.sin(theta) * length;
+            const xRadius = Math.cos(theta) * length;
+            const x = offset + xRadius
+            const y = offset + yRadius;
+            const invertedY = offset - yRadius;
+            invertedPoints.push([x, invertedY])
+            acc.push([x, y]);
+          }
+          return acc;
+        }, []);
+        points = points.concat(invertedPoints.reverse());
+        const redPoints = points.reduce((acc, value) => {
+          const x = ((value[0] - offset)) + offset
+          const y = ((value[1] - offset)) + offset
+          acc.push([x, y]);
+          return acc;
+        }, []);
+        const redPath = spline(redPoints, 1, true);
 
-      let blackSkew = (blueFrequency / 500) * (blackRatio * ratio);
-      blackSkew = blackSkew - (blackSkew % 5)
-      if (blackSkew > maxDistortionBlack) blackSkew = maxDistortionBlack;
 
-      let redSkew = (redFrequency / 500) * (redRatio * ratio);
-      redSkew = redSkew - (redSkew % 5)
-      if (redSkew > maxDistortionRed) redSkew = maxDistortionRed;
-      //console.log(blackSkew, blueSkew, redSkew)
+        const whitePoints = points.reduce((acc, value) => {
+          const x = ((value[0] - offset) / 1.2) + offset
+          const y = ((value[1] - offset) / 1.2) + offset
+          acc.push([x, y]);
+          return acc;
+        }, [])
+        const whitePath = spline(whitePoints, 1, true);
+        redHistory.push(redPath);
 
-      blackCircles.css({
-        transform: 'skewX(' + blackSkew +'deg)' + 'skewY(' + blackSkew + 'deg)'
-      });
-      blueCircles.css({
-        transform: 'skewX(' + blueSkew + 'deg)' + 'skewY(' + blueSkew + 'deg)'
-      });
-      redCircles.css({
-        transform: 'skewX(' + redSkew + 'deg)' + 'skewY(' + redSkew + 'deg)'
-      });
+        redMask.children("path").attr('d', redPath)
+        whiteMask.children("path").attr('d', whitePath)
+
+        if (redHistory.length >= 30) {
+          const bluePath = redHistory.splice(0, 1);
+          blueMask.children("path").attr('d', bluePath)
+        }
+        //delayPath(white, path, 200);
     }
     doDraw();
   }
 
   var soundNotAllowed = function (error) {
-    h.innerHTML = "You must allow your microphone.";
-    console.log(error);
+      h.innerHTML = "You must allow your microphone.";
+      console.log(error);
   }
 
-  window.navigator = window.navigator || {};
-  navigator.getUserMedia =  navigator.getUserMedia       ||
+  /*window.navigator = window.navigator || {};
+  /*navigator.getUserMedia =  navigator.getUserMedia       ||
                             navigator.webkitGetUserMedia ||
                             navigator.mozGetUserMedia    ||
-                            null;
-  navigator.getUserMedia({ audio: true }, soundAllowed, soundNotAllowed);
+                            null;*/
+  navigator.getUserMedia({audio:true}, soundAllowed, soundNotAllowed);
 
 };
